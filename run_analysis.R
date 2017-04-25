3352e7a23d302361b891355008c1d0cc48a72c26
Gonçalo Nogueira <jgoncalonogueira@gmail.com>
2017-04-25 09:20:11 +0100

    Added the outline of the script in comments.

---
+++ run_analysis.R
@@ -0,0 +1,35 @@
+# Load all the data
+
+# Load the list of features in the feature vector
+
+# Load the activity labels
+
+# For the train and test datasets, load the feature vector, the labels
+# and the subjects.
+# The inertial signals will not be needed
+
+
+# Merge the test and train datasets
+
+# For the train and test datasets, construct a dataframe with the following
+# columns:
+#
+# subject
+# activity
+# feature vector (one feature per column)
+
+# Merge the two dataframes previously created by appending the rows of one
+# to another.
+
+# Use the list of features to properly name the dataframe's columns
+
+# Subset the dataframe to retain only the columns that contain means
+# or standard deviations
+
+# Use the activity labels to substitute the integers in the activity
+# column of the dataframe for descriptive activity labels
+
+# Group the dataframe by subject and by activity
+
+# Summarize the grouped dataset by computing the mean of each of the
+# features
